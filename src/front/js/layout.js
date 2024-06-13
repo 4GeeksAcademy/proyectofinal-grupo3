93e4bf397de4313e302ee5dc527c016bcdb7177b
LOLA-1980 <109870340+LOLA-1980@users.noreply.github.com>
2024-06-13 06:33:01 +0000

    [Desarrollo de componente DoctorsDirectory, SearchBar y ContactSection]

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,8 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import DoctorsDirectory  from "./component/DoctorsDirectory.jsx";
+import ContactSection from "./component/ContactSection.jsx";
 
 //create your first component
 const Layout = () => {
@@ -28,6 +30,8 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route path="/doctors" element={<DoctorsDirectory />} />
+                        <Route path="/contact" element={<ContactSection />} /> {/* Agrega esta ruta para el contacto */}
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
